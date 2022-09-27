6319c2e9165caf106d7462b8772d33863e9f37dd
YunnisaDiahPratiwi <112565636+YunnisaDiahPratiwi@users.noreply.github.com>
2022-09-27 20:24:41 +0700

    Add files via upload

---
+++ Yunnisa Diah Pratiwi_2217051078_praktest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int angka;
+	cin>>angka;
+	if(angka >1>=3){
+		cout<<"tiga"<<endl;
+	}else{
+		cout<<"bilangan lebih besar dari tiga"<<endl;
+	}
+}
